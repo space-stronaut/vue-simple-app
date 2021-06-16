b30246cbcefecc02d2d839106272fb8dc5104cf5
Spacestronaut <Abelr6099@gmail.com>
2021-06-16 11:16:46 +0700

    todo commit

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const App = {
-    data() {
-        return {
-            color: ""
-        }
-    },
-}
-
-Vue.createApp(App).mount("#root")
+const app = Vue.createApp({})
+app.component('todo', {
+  data() {
+    return {
+      todos: [],
+      value: ""
+    }
+  },
+  methods: {
+      handleSubmit(e){
+          e.preventDefault();
+            this.todos.push(this.value)
+            this.value = ""
+      }
+  },
+  template: `
+  <form v-on:submit="handleSubmit">
+    <input v-model="value"/>
+    <button type="submit">Submit</button>
+  </form>
+    
+  <ul v-for="todo in todos">
+    <li>{{todo}}</li>
+  </ul>
+  `
+})
+app.mount('#root')
