4e8a5eaf3d6a0ee36c2fd07667d104896d6b4f5e
Spacestronaut <Abelr6099@gmail.com>
2021-06-16 13:38:24 +0700

    delete commit

--- app.js
+++ app.js
@@ -11,19 +11,22 @@ app.component('Todo', {
           e.preventDefault();
             this.todos.push(this.value)
             this.value = ""
+      },
+      hapus(e){
+            delete this.todos[e]
       }
   },
   template: `
-  <form v-on:submit="handleSubmit" class="d-flex mt-5">
-    <input v-model="value" placeholder="Masukkan Kegiatan" class="form-control"/>
+  <form v-on:submit="handleSubmit" class="d-flex mt-5 ">
+    <input v-model="value" placeholder="Masukkan Kegiatan" style="width : 300px" class="form-control" required/>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
     
-  <div v-for="todo in todos" class="mb-2 mt-3">
-    <div class="alert alert-primary" role="alert">
-        {{todo}}
+  <div v-for="(value,index) in todos" class="mb-2 mt-3 d-flex justify-content-evenly">
+    <div class="alert alert-primary alert-dismissible fade show" style="width : 300px;" role="alert">
+        {{value}}
+        <button v-on:click="hapus(index)" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   </div>
   `
-})
-app.mount('#root')
+}).mount('#root')
